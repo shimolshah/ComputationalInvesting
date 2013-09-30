2441270038d5da2be5e8602d2832fb85c8a637d7
Shimol Shah <shimol_shah@yahoo.com>
2013-09-29 18:19:06 -0700

    first revision

--- HW3/marketsim.py
+++ HW3/marketsim.py
@@ -15,6 +15,8 @@ uniqueDates=[]
 uniqueSymboles=[]
 d_data=[]
 trade_matrix=[]
+holding_matrix=[]
+values_matrix=[]
 
 def test_msg():
     print "Hello"
@@ -43,6 +45,7 @@ def preprocess_raw_data():
     uniqueSymbols = list(set(symbols))
 
 def perform_step2():
+    global uniqueDates
     uniqueDates.sort()
     # print uniqueDates
 
@@ -68,19 +71,20 @@ def perform_step2():
         d_data[s_key] = d_data[s_key].fillna(1.0)
 
     d_data['_CASH'] = 1.0
+    uniqueDates = [0] + uniqueDates
+
     #pd.set_printoptions(max_columns=7)
     #print d_data['close'].values
     #d_data.to_csv(sys.stdout)
     
     #print d_data['close']['_CASH']
-    print d_data['close']['AAPL']
-    print d_data['close']['AAPL'][0]
-    print d_data['close']['AAPL'][1]
-    print d_data['close']['AAPL']["2011-01-12 16:00:00"]
+    #print d_data['close']['AAPL']
+    #print d_data['close']['AAPL'][0]
+    #print d_data['close']['AAPL'][1]
+    #print d_data['close']['AAPL']["2011-01-12 16:00:00"]
 
 def generate_trade_matrix(capital):
     global uniqueDates
-    uniqueDates = [0] + uniqueDates
     dt_start = uniqueDates[0]
     dt_end = uniqueDates[-1] + dt.timedelta(days=1)
 
@@ -97,19 +101,76 @@ def generate_trade_matrix(capital):
     trade_matrix.loc[0]["_CASH"] = capital
     previous_date = 0
     for date in uniqueDates:
-        print "-", date
-        trade_matrix.loc[date]["_CASH"] = trade_matrix.loc[previous_date]["_CASH"]
+        #print "-", date
+        if(date != 0):
+            trade_matrix.loc[date]["_CASH"] = 0
         for row in rawdata:
             if(dt.datetime(int(row[0]),int(row[1]),int(row[2])) == date):
-                print "-", row[3],"-", row[4],"-", row[5]
-                print d_data['close'][row[3]][date+dt.timedelta(hours=16)]
-                trade_matrix.loc[date][row[3]] = \
-                    int(row[5]) if row[4] == "Buy" else -1 * int(row[5])
+                # print "-", row[3],"-", row[4],"-", row[5]
+                # print d_data['close'][row[3]][date+dt.timedelta(hours=16)]
+                a = trade_matrix.loc[date][row[3]]
+                b = int(row[5]) if row[4] == "Buy" else -1 * int(row[5])
+
+                #if(row[3] == 'GOOG'):
+                #    print date
+                #    print a
+                #    print b
+                #    print a + b
+                    
+                trade_matrix.loc[date][row[3]] = a + b
+                
+                #if(row[3] == 'GOOG'):
+                #    print trade_matrix.loc[date][row[3]]
+
                 trade_matrix.loc[date]["_CASH"] = \
                     trade_matrix.loc[date]["_CASH"] - \
                         trade_matrix.loc[date][row[3]] * d_data['close'][row[3]][date+dt.timedelta(hours=16)]
                 #print trade_matrix.loc[previous_date]["_CASH"]
         previous_date = date    
+
+def generate_holding_matrix():
+    global uniqueDates
+    uniqueDates = uniqueDates
+
+    global holding_matrix
+    holding_matrix = pd.DataFrame(index=uniqueDates, columns=uniqueSymbols)
+    holding_matrix = holding_matrix.fillna(0)
+
+    #global trade_matrix
+    #print uniqueDates[1]
+    #print trade_matrix.loc[uniqueDates[1]].cumsum()
+    #df = []
+    print "--"
+    for symbol in uniqueSymbols:
+        holding_matrix[symbol]=trade_matrix[symbol].cumsum()
+
+def calculate_returns():
+    global values_matrix
+    values_matrix = pd.DataFrame(index=uniqueDates, columns=uniqueSymbols)
+    values_matrix = values_matrix.fillna(0)
+
+    for date in uniqueDates:
+        c = 0
+        if date != 0:
+            #print d_data['close'].loc[date+dt.timedelta(hours=16)]
+            #print holding_matrix.loc[date]
+            a = d_data['close'].loc[date+dt.timedelta(hours=16)]
+            b = holding_matrix.loc[date]
+            c += a.mul(b)
+            values_matrix.loc[date]=c
+            #print c
+        else:
+            values_matrix.loc[date]=holding_matrix.loc[0]
+            #print
+
+def write_csv(filename):
+    writer = csv.writer(open(filename, 'wb'), delimiter=',')
+    for date in uniqueDates:
+        if(date!=0):
+            val = values_matrix.loc[date].cumsum()['_CASH']
+            list_a = [date.year, date.month, date.day, int(val)]
+            print list_a
+            writer.writerow(list_a)
         
 if __name__ == '__main__':
     test_msg()
@@ -118,10 +179,17 @@ if __name__ == '__main__':
     # print rawdata
     preprocess_raw_data()
     # print uniqueDates
-    print ""
-    print uniqueSymbols
+    #print ""
+    #print uniqueSymbols
     perform_step2()
     # print d_data['close']['AAPL']
     generate_trade_matrix(int(sys.argv[1]))
+    print "Trading Matrix"
     print trade_matrix
-    
+    generate_holding_matrix()
+    print "Holding Matrix"
+    print holding_matrix
+    calculate_returns()
+    print "Value Matrix"
+    print values_matrix
+    write_csv(sys.argv[3])
